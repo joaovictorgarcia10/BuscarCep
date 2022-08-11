6e0c9f7e2394041320ae93d9d05e1181053236aa
Joao Santana <joao.santana@rarolabs.com.br>
2022-08-11 09:42:17 -0300

    add auth datasource tests

--- test/modules/auth/data/datasources/auth_datasource_test.dart
+++ test/modules/auth/data/datasources/auth_datasource_test.dart
@@ -1,25 +1,124 @@
-// TODO: implementar esses testes da melhor forma (dependem do firebase)
-
+import 'package:clean_arch_aula/modules/auth/data/datasources/auth_datasource.dart';
+import 'package:clean_arch_aula/modules/auth/data/models/user_model.dart';
+import 'package:clean_arch_aula/shared/core/error/failure.dart';
+import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockAuthDatasourceImpl extends Mock implements AuthDatasource {}
 
 void main() {
-  setUp(() {});
+  late AuthDatasource datasource;
+
+  setUp(() async {
+    datasource = MockAuthDatasourceImpl();
+  });
 
   group("doLogin", () {
-    test("doLogin - success", () async {});
+    test("doLogin - success", () async {
+      final user = UserModel();
 
-    test("doLogin - failure", () {});
+      when(
+        () => datasource.doLogin(email: "email", password: "senha"),
+      ).thenAnswer((_) async => Right(user));
+
+      final result = await datasource.doLogin(
+        email: "email",
+        password: "senha",
+      );
+
+      result.fold(
+        (l) => null,
+        (r) => expect(r, user),
+      );
+    });
+
+    test("doLogin - failure", () async {
+      final failure = Failure();
+
+      when(
+        () => datasource.doLogin(email: "email", password: "senha"),
+      ).thenAnswer((_) async => Left(failure));
+
+      final result = await datasource.doLogin(
+        email: "email",
+        password: "senha",
+      );
+
+      result.fold(
+        (l) => expect(l, failure),
+        (r) => null,
+      );
+    });
   });
 
   group("createAccount", () {
-    test("createAccount - failure", () {});
+    test("createAccount - failure", () async {
+      when(
+        () => datasource.createAccount(email: "email", password: "senha"),
+      ).thenAnswer((_) async => const Right(true));
 
-    test("createAccount - success", () {});
+      final result = await datasource.createAccount(
+        email: "email",
+        password: "senha",
+      );
+
+      result.fold(
+        (l) => null,
+        (r) => expect(r, true),
+      );
+    });
+
+    test("createAccount - success", () async {
+      final failure = Failure();
+
+      when(
+        () => datasource.createAccount(email: "email", password: "senha"),
+      ).thenAnswer((_) async => Left(failure));
+
+      final result = await datasource.createAccount(
+        email: "email",
+        password: "senha",
+      );
+
+      result.fold(
+        (l) => expect(l, failure),
+        (r) => null,
+      );
+    });
   });
 
   group("resetPassword", () {
-    test("resetPassword - failure", () {});
+    test("resetPassword - failure", () async {
+      when(
+        () => datasource.resetPassword(email: "email"),
+      ).thenAnswer((_) async => const Right(true));
 
-    test("resetPassword - success", () {});
+      final result = await datasource.resetPassword(
+        email: "email",
+      );
+
+      result.fold(
+        (l) => null,
+        (r) => expect(r, true),
+      );
+    });
+
+    test("resetPassword - success", () async {
+      final failure = Failure();
+
+      when(
+        () => datasource.resetPassword(email: "email"),
+      ).thenAnswer((_) async => Left(failure));
+
+      final result = await datasource.resetPassword(
+        email: "email",
+      );
+
+      result.fold(
+        (l) => expect(l, failure),
+        (r) => null,
+      );
+    });
   });
 }
