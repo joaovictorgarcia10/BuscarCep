293b52471e62e1d9ebc0c241c482f7730a3c3c58
Joao Santana <joao.santana@rarolabs.com.br>
2022-10-29 15:24:44 -0300

    fix

--- lib/shared/core/custom_usecase/custom_usecase.dart
+++ lib/shared/core/custom_usecase/custom_usecase.dart
@@ -1,10 +1,3 @@
-import 'package:dartz/dartz.dart';
-import '../error/failure.dart';
-
-abstract class UsecaseCore<T, Params> {
-  Future<Either<Failure, T>> call(Params params);
-}
-
 abstract class StreamUseCase<Type, Params> {
   Stream<Type> call(Params params);
 }
