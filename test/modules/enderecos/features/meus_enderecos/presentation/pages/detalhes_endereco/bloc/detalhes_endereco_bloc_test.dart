2698e43cbbfe5a72165825c5419bc21cf250f339
joao.santana <joao.santana@globalhitss.com.br>
2023-04-11 15:27:25 -0300

    add bloc tests

--- test/modules/enderecos/features/meus_enderecos/presentation/pages/detalhes_endereco/bloc/detalhes_endereco_bloc_test.dart
+++ test/modules/enderecos/features/meus_enderecos/presentation/pages/detalhes_endereco/bloc/detalhes_endereco_bloc_test.dart
@@ -1,7 +1,119 @@
+import 'package:buscar_cep/modules/enderecos/features/meus_enderecos/domain/repositories/meus_enderecos_repository.dart';
+import 'package:buscar_cep/modules/enderecos/features/meus_enderecos/domain/usecases/delete_endereco.dart';
+import 'package:buscar_cep/modules/enderecos/features/meus_enderecos/domain/usecases/get_geolocation.dart';
+import 'package:buscar_cep/modules/enderecos/features/meus_enderecos/presentation/pages/detalhes_endereco/bloc/detalhes_endereco_bloc.dart';
+import 'package:buscar_cep/modules/enderecos/features/meus_enderecos/presentation/pages/detalhes_endereco/bloc/detalhes_endereco_event.dart';
+import 'package:buscar_cep/modules/enderecos/features/meus_enderecos/presentation/pages/detalhes_endereco/bloc/detalhes_endereco_state.dart';
+import 'package:buscar_cep/shared/core/error/failure.dart';
+import 'package:buscar_cep/shared/utils/geolocation/geolocation.dart';
+import 'package:buscar_cep/shared/utils/geolocation/geolocation_service.dart';
+import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockMeusEnderecosRepository extends Mock
+    implements MeusEnderecosRepository {}
+
+class MockGeolocationService extends Mock implements GeolocationService {}
 
 void main() {
-  testWidgets('detalhes endereco bloc ...', (tester) async {
-    // TODO: Implement test
+  late GeolocationService geolocationService;
+  late MeusEnderecosRepository repository;
+
+  late DeleteEndereco deleteEndereco;
+  late GetGeolocation getGeolocation;
+
+  late DetalhesEnderecoBloc bloc;
+
+  setUp(() {
+    geolocationService = MockGeolocationService();
+    repository = MockMeusEnderecosRepository();
+
+    deleteEndereco = DeleteEndereco(repository);
+    getGeolocation = GetGeolocation(geolocationService);
+
+    bloc = DetalhesEnderecoBloc(deleteEndereco, getGeolocation);
+  });
+
+  group("GetGeolocation", () {
+    test("GetGeolocation - success", () {
+      final geolocation = Geolocation(latitude: 1.0, longitude: 2.0);
+
+      when(() => geolocationService.getGeolocation())
+          .thenAnswer((_) async => Right(geolocation));
+
+      bloc.add(const DetalhesEnderecoEvent.getGeolocation());
+
+      expectLater(
+        bloc.stream,
+        emitsInOrder(
+          [
+            const DetalhesEnderecoState.loading(),
+            DetalhesEnderecoState.getGeoLocationSuccess(
+              geolocation: geolocation,
+            ),
+          ],
+        ),
+      );
+    });
+
+    test("GetGeolocation - failure", () {
+      final failure = Failure();
+
+      when(() => geolocationService.getGeolocation())
+          .thenAnswer((_) async => Left(failure));
+
+      bloc.add(const DetalhesEnderecoEvent.getGeolocation());
+
+      expectLater(
+        bloc.stream,
+        emitsInOrder(
+          [
+            const DetalhesEnderecoState.loading(),
+            DetalhesEnderecoState.failure(failure: failure),
+          ],
+        ),
+      );
+    });
+  });
+
+  group("DeleteEndereco", () {
+    test("DeleteEndereco - success", () {
+      when(() => repository.deleteEndereco(idEndereco: "01"))
+          .thenAnswer((_) async => const Right(true));
+
+      bloc.add(DetalhesEnderecoEvent.deleteEndereco(
+          params: DeleteEnderecoParams(id: "01")));
+
+      expectLater(
+        bloc.stream,
+        emitsInOrder(
+          const [
+            DetalhesEnderecoState.loading(),
+            DetalhesEnderecoState.deleteEnderecoSuccess(),
+          ],
+        ),
+      );
+    });
+
+    test("DisconnectAccount - failure", () {
+      final failure = Failure();
+
+      when(() => repository.deleteEndereco(idEndereco: "01"))
+          .thenAnswer((_) async => Left(failure));
+
+      bloc.add(DetalhesEnderecoEvent.deleteEndereco(
+          params: DeleteEnderecoParams(id: "01")));
+
+      expectLater(
+        bloc.stream,
+        emitsInOrder(
+          [
+            const DetalhesEnderecoState.loading(),
+            DetalhesEnderecoState.failure(failure: failure),
+          ],
+        ),
+      );
+    });
   });
 }
--- test/modules/enderecos/features/meus_enderecos/presentation/pages/meus_enderecos/bloc/meus_enderecos_bloc_test.dart
+++ test/modules/enderecos/features/meus_enderecos/presentation/pages/meus_enderecos/bloc/meus_enderecos_bloc_test.dart
@@ -1,7 +1,67 @@
+import 'package:buscar_cep/modules/enderecos/features/meus_enderecos/domain/repositories/meus_enderecos_repository.dart';
+import 'package:buscar_cep/modules/enderecos/features/meus_enderecos/domain/usecases/get_lista_enderecos.dart';
+import 'package:buscar_cep/modules/enderecos/features/meus_enderecos/presentation/pages/meus_enderecos/bloc/meus_enderecos_bloc.dart';
+import 'package:buscar_cep/modules/enderecos/features/meus_enderecos/presentation/pages/meus_enderecos/bloc/meus_enderecos_event.dart';
+import 'package:buscar_cep/modules/enderecos/features/meus_enderecos/presentation/pages/meus_enderecos/bloc/meus_enderecos_state.dart';
+import 'package:buscar_cep/modules/enderecos/shared/entities/endereco.dart';
+import 'package:buscar_cep/shared/core/error/failure.dart';
+import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockMeusEnderecosRepository extends Mock
+    implements MeusEnderecosRepository {}
 
 void main() {
-  testWidgets('meus enderecos bloc ...', (tester) async {
-    // TODO: Implement test
+  late MeusEnderecosRepository repository;
+  late GetListaEnderecos getListaEnderecos;
+  late MeusEnderecosBloc bloc;
+
+  setUp(() {
+    repository = MockMeusEnderecosRepository();
+    getListaEnderecos = GetListaEnderecos(repository);
+    bloc = MeusEnderecosBloc(getListaEnderecos);
+  });
+
+  group("DeleteEndereco", () {
+    test("DeleteEndereco - success", () {
+      final enderecos = <Endereco>[];
+
+      when(() => repository.getListaEnderecos())
+          .thenAnswer((_) async => Right(enderecos));
+
+      bloc.add(const MeusEnderecosEvent.getListaEnderecos());
+
+      expectLater(
+        bloc.stream,
+        emitsInOrder(
+          [
+            const MeusEnderecosState.loading(),
+            MeusEnderecosState.getListaEnderecosSuccess(
+              listaEnderecos: enderecos,
+            ),
+          ],
+        ),
+      );
+    });
+
+    test("DisconnectAccount - failure", () {
+      final failure = Failure();
+
+      when(() => repository.getListaEnderecos())
+          .thenAnswer((_) async => Left(failure));
+
+      bloc.add(const MeusEnderecosEvent.getListaEnderecos());
+
+      expectLater(
+        bloc.stream,
+        emitsInOrder(
+          [
+            const MeusEnderecosState.loading(),
+            MeusEnderecosState.failure(failure: failure),
+          ],
+        ),
+      );
+    });
   });
 }
